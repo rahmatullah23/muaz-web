213785e2281329ff775d2ab59e07aa21ed3d5c25
rahmatullah23 <tomalg@gmail.com>
2021-01-23 08:52:51 +0300

    last commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -32,13 +32,12 @@ const Home = () => {
                         <button type="button" class="btn btn-warning p-1" onClick={() => setImage(nanu)} >Nanu</button>
                     </li>
                     <li class="nav-item p-1">
-                        <button type="button" class="btn btn-primary" onClick={() => setImage(m1)} >Tareq</button>
+                        <button type="button" class="btn btn-primary p-1" onClick={() => setImage(m1)} >Tareq</button>
                     </li>
                     <li class="nav-item p-1">
-                        <button type="button" class="btn btn-success" onClick={() => setImage(m2)} >Zaber</button>
+                        <button type="button" class="btn btn-success p-1" onClick={() => setImage(m2)} >Zaber</button>
                     </li>
                 </ul>
-
                 <div class="col-sm-12">
                     <img class="img-fluid" src={image} alt="" />
                 </div>
